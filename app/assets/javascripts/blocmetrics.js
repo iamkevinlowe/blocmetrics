f2d6ab18bb77142e0be5437446e1ddd6aa19ae0e
Kevin Lowe <iamkevinlowe@gmail.com>
2015-08-06 15:37:58 -0700

    Added blocmetrics.js

---
+++ app/assets/javascripts/blocmetrics.js
@@ -0,0 +1,18 @@
+var blocmetrics = {
+  report: function(event_name) {
+    var _bm_event = {
+      name: event_name
+    };
+    var _bm_url = "http://iamkevinlowe-blocmetrics.herokuapp.com/api/events";
+
+    var _bm_request = new XMLHttpRequest();
+    _bm_request.open("POST", _bm_url, true);
+    _bm_request.setRequestHeader('Content-Type', 'application/json');
+    _bm_request.onreadystatechange = function () {
+    };
+    _bm_request.send(JSON.stringify(_bm_event));
+  }
+};
+
+// Add "//= require blocmetrics" in your application.js file
+// To create a metric, call "blocmetrics.report([Metric Name])"
